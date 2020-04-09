09b5a54ad1bb6865e92ed9f5d69ea6104b141c7e
Regan <Regan@LAPTOP-UIL8RSD4>
2020-04-08 21:59:30 -0500

    Finished exam question 1

--- Data/Cobbler.cs
+++ Data/Cobbler.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ExamTwoCodeQuestions.Data
 {
-    public class Cobbler : IOrderItem
+    public class Cobbler : IOrderItem, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Event handler for implementing INotifyPropertyChanged interface
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private FruitFilling fruit;
         /// <summary>
         /// The fruit used in the cobbler
         /// </summary>
-        public FruitFilling Fruit { get; set; }
-
+        public FruitFilling Fruit
+        {
+            get => fruit;
+            set
+            {
+                fruit = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Fruit"));
+            }
+        }
+        private bool withIceCream = true;
         /// <summary>
         /// If the cobbler is served with ice cream
         /// </summary>
-        public bool WithIceCream { get; set; } = true;
-
+        public bool WithIceCream
+        {
+            get => withIceCream;
+            set
+            {
+                withIceCream = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("WithIceCream"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
         /// <summary>
         /// Gets the price of the Cobbler
         /// </summary>
@@ -38,5 +61,7 @@ namespace ExamTwoCodeQuestions.Data
                 else { return new List<string>() { "Hold Ice Cream" }; }
             }
         }
+
+        
     }
 }
